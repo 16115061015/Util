e247c9e6929a096f00437e7bbaa3312539f23a42
huzeyu <hzy-joel@qq.com>
2020-06-25 17:06:39 +0800

    feat(初始化BVH模块): BVH学习

---
+++ rollscreenview/src/main/java/com/hzy/rollscreenview/RollScreenLayout.kt
@@ -0,0 +1,18 @@
+package com.hzy.rollscreenview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+/**
+ * User: hzy
+ * Date: 2020/6/25
+ * Time: 4:45 PM
+ * Description: 滚动控件
+ */
+class RollScreenLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
+    init {
+
+    }
+    //
+}
---
+++ rollscreenview/src/main/java/com/hzy/rollscreenview/adapter/RollScreenAdapter.kt
@@ -0,0 +1,12 @@
+package com.hzy.rollscreenview.adapter
+
+/**
+ * User: hzy
+ * Date: 2020/6/25
+ * Time: 4:52 PM
+ * Description: 滚动公屏适配器
+ */
+class RollScreenAdapter<T> {
+    var datas: List<T>? = null
+
+}
