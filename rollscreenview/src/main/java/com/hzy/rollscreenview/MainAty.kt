8b537f6fcbaf5c16557709569587eb01d99be141
huzeyu <hzy-joel@qq.com>
2020-06-26 20:48:01 +0800

    feat(滚动公屏控件封装)

--- rollscreenview/src/main/java/com/hzy/rollscreenview/MainAty.kt
+++ rollscreenview/src/main/java/com/hzy/rollscreenview/MainAty.kt
@@ -1,8 +1,11 @@
 package com.hzy.rollscreenview
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * User: hzy
@@ -10,10 +13,42 @@ import androidx.appcompat.app.AppCompatActivity
  * Time: 4:27 PM
  * Description:
  */
-class MainAty : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class MainAty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = MainAdapter()
+        rl.adapter = adapter
+        adapter.setNewData(mutableListOf(
+                DataBean(0, "123"),
+                DataBean(1, "abc"),
+                DataBean(0, "123")))
+
     }
 }
+
+class MainAdapter : RollScreenLayout.Adapter<DataBean>() {
+    override fun onCreateView(vp: ViewGroup, data: DataBean): View? {
+        return when (data.type) {
+            0 -> return View.inflate(vp.context, R.layout.rollscreen_item1, null)
+            1 -> return View.inflate(vp.context, R.layout.rollscreen_item2, null)
+            else -> TextView(vp.context)
+        }
+    }
+
+
+    override fun getItemType(data: DataBean): Int {
+        return data.type
+    }
+
+    override fun convert(data: DataBean, holder: RollScreenLayout.ViewHolder) {
+        when (data.type) {
+            0 -> holder.setText(R.id.tv, data.content)
+            1 -> holder.setText(R.id.textView, data.content)
+        }
+    }
+
+}
+
+class DataBean(val type: Int, val content: String)
 
--- rollscreenview/src/main/java/com/hzy/rollscreenview/RollScreenLayout.kt
+++ rollscreenview/src/main/java/com/hzy/rollscreenview/RollScreenLayout.kt
@@ -2,7 +2,14 @@ package com.hzy.rollscreenview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.SparseArray
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.ViewFlipper
+import androidx.annotation.StringRes
+import java.util.*
 
 /**
  * User: hzy
@@ -10,9 +17,86 @@ import android.widget.LinearLayout
  * Time: 4:45 PM
  * Description: 滚动控件
  */
-class RollScreenLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
-    init {
+class RollScreenLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : ViewFlipper(context, attrs) {
+    var adapter: Adapter<*>? = null
+        set(value) {
+            adapter?.dataChangesListener = {
+                showNext()
+            }
+            field = value
+            showNext()
+        }
+
+    override fun showNext() {
+        adapter?.getNextItemView(this)?.let {
+            addView(it.itemView)
+            super.showNext()
+        }
+    }
+
+
+    abstract class Adapter<T> {
+        private val data = LinkedList<T>()
+        private val viewsCache = SparseArray<ViewHolder>()
+        var dataChangesListener: () -> Unit? = {}
+
+        fun setNewData(data: List<T>) {
+            this.data.addAll(data)
+            dataChangesListener.invoke()
+        }
+
+        private fun onCreateViewHolder(vp: ViewGroup, data: T): ViewHolder? {
+            return onCreateView(vp, data)?.let { ViewHolder(it) }
+        }
+
+        abstract fun onCreateView(vp: ViewGroup, data: T): View?
+
+        fun getNextItemView(vp: ViewGroup): ViewHolder? {
+            if (data.isNullOrEmpty()) return null
+            val data = data.poll()
+            var viewHolder = viewsCache.get(getItemType(data))
+            if (viewHolder == null) {
+                viewHolder = onCreateViewHolder(vp, data)
+                viewsCache.put(getItemType(data), viewHolder)
+            }
+            viewHolder?.let {
+                viewHolder.itemView.parent?.let { (it as ViewGroup).removeView(viewHolder.itemView) }
+                convert(data, viewHolder)
+            }
+            return viewHolder
+        }
+
+        open fun getItemType(data: T): Int = 0
+
+        abstract fun convert(data: T, holder: ViewHolder)
+    }
+
+    class ViewHolder(var itemView: View) {
+        private val viewCache = SparseArray<View>()
+
+        fun setText(id: Int, @StringRes stringId: Int) {
+            (getView(id) as TextView).text = itemView.context.getString(stringId)
+        }
+
+        fun setText(id: Int, string: String) {
+            (getView(id) as TextView).text = string
+        }
+
+        fun setOnClick(id: Int, onClick: () -> Unit) {
+            getView(id).setOnClickListener {
+                onClick()
+            }
+        }
+
+        private fun getView(id: Int): View {
+            var view = viewCache.get(id)
+            if (view == null) {
+                view = itemView.findViewById(id)
+                viewCache.put(id, view)
+            }
+            return view
+        }
 
     }
-    //
 }
+
