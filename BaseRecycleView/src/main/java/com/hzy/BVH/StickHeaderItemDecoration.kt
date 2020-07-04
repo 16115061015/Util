cb8ce62034b38987f7d1c61cc02a7ea27d57ff79
huzeyu <hzy-joel@qq.com>
2020-07-05 01:36:01 +0800

    fix(自定义ItemDecoration):粘性头部完善切换动画

--- BaseRecycleView/src/main/java/com/hzy/BVH/StickHeaderItemDecoration.kt
+++ BaseRecycleView/src/main/java/com/hzy/BVH/StickHeaderItemDecoration.kt
@@ -5,10 +5,8 @@ import android.graphics.Rect
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.hzy.BVH.helper.dp
-import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
 /**
@@ -52,14 +50,14 @@ class StickHeaderItemDecoration(private val layoutId: Int, private val stickView
         val right: Int = parent.width - parent.paddingRight
         for (position in 0 until parent.childCount) {
             val child = parent.getChildAt(position)
-            val dataPosition = parent.getChildAdapterPosition(child)
-            if (transfer?.isFirst(dataPosition) == true) {
+            val adapterPosition = parent.getChildAdapterPosition(child)
+            if (transfer?.isFirst(adapterPosition) == true) {
                 //计算ItemView的实际top
                 val bottom = getViewRealTop(child)
                 val top = bottom - stickViewHeight.dp
                 val view = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
                 //先绑定数据再测量
-                transfer?.bindData(view, dataPosition)
+                transfer?.bindData(view, adapterPosition)
                 view.measure(child.measuredWidth, child.measuredHeight)
                 view.layout(left, top, right, bottom)
                 c.translate(0.toFloat(), top.toFloat())
@@ -79,30 +77,35 @@ class StickHeaderItemDecoration(private val layoutId: Int, private val stickView
          */
         val left: Int = parent.paddingLeft
         val right: Int = parent.width - parent.paddingRight
-        val position = 0
-        val child = parent.getChildAt(position)
-        val dataPosition = parent.getChildAdapterPosition(child)
-        val params = child
-                .layoutParams as RecyclerView.LayoutParams
-
-        //如果是最后一个的话需要随着最后一个的top变小而往上走
+        val child = parent.getChildAt(0)
+        val adapterPosition = parent.getChildAdapterPosition(child)
         val bottom = getViewRealTop(child)
         val top = bottom - stickViewHeight.dp
         //先绑定数据再测量
-        transfer?.bindData(stickHeadView, dataPosition)
+        transfer?.bindData(stickHeadView, adapterPosition)
         stickHeadView.measure(child.measuredWidth, child.measuredHeight)
         stickHeadView.layout(left, top, right, bottom)
-        if (transfer?.isLast(dataPosition) == true) {
-            val progress = top.absoluteValue % (stickViewHeight.dp + params.height)
-            c.translate(0.toFloat(), (params.height * progress).toFloat())
-            stickHeadView.draw(c)
-            c.translate(0.toFloat(), (-params.height * progress).toFloat())
-        } else {
-            stickHeadView.draw(c)
-        }
+        if (!beginTransfer(c, parent)) stickHeadView.draw(c)
 
     }
 
+
+    private fun beginTransfer(c: Canvas, parent: RecyclerView): Boolean {
+        //判断分组的最后一个距离头部的距离判断是否开始移动
+        for (position in 0 until parent.childCount) {
+            val view = parent.getChildAt(position)
+            if (transfer?.isLast(parent.getChildAdapterPosition(view)) == true) {
+                val top = getViewRealTop(view)
+                val distance = top + view.height
+                if (distance > stickViewHeight.dp) return false
+                c.translate(0.toFloat(), (top - view.height).toFloat())
+                stickHeadView.draw(c)
+                c.translate(0.toFloat(), -(top - view.height).toFloat())
+                return true
+            }
+        }
+        return false
+    }
 
     private fun getViewRealTop(child: View): Int {
         val params = child
