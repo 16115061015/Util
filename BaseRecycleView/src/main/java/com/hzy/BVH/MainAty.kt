ec7bd357c0ae3324dbf2cd4a099d668524282db8
huzeyu <hzy-joel@qq.com>
2020-07-07 17:15:14 +0800

    fix(RecycleView):item点击时缩小

--- BaseRecycleView/src/main/java/com/hzy/BVH/MainAty.kt
+++ BaseRecycleView/src/main/java/com/hzy/BVH/MainAty.kt
@@ -1,8 +1,12 @@
 package com.hzy.BVH
 
 import android.os.Bundle
+import android.view.KeyEvent.ACTION_DOWN
+import android.view.MotionEvent
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.hzy.BVH.adapter.BaseAdapter
 import com.hzy.BVH.adapter.BaseVH
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,6 +29,26 @@ class MainAty : AppCompatActivity() {
                 DataBean("789", 1),
                 DataBean("dasd", 1),
                 DataBean("fadasd", 0)))
+        rv.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+            }
+
+            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                //点击时缩小
+                val childView = rv.findChildViewUnder(e.x, e.y)
+                if (e.action == ACTION_DOWN) {
+                    childView?.scaleY = 0.8F
+                } else {
+                    childView?.scaleY = 1F
+                }
+                return false
+            }
+
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+
+            }
+
+        })
     }
 
 }
@@ -36,6 +60,12 @@ class MainAdapter : BaseAdapter<DataBean>() {
                 holder.setText(R.id.tv, data.title)
                 holder.setOnClick(R.id.tv) {
                     Toast.makeText(holder.itemView.context, data.title, Toast.LENGTH_LONG).show()
+                }
+                holder.itemView.findViewById<TextView>(R.id.tv).setOnFocusChangeListener { _, hasFocus ->
+                    run {
+                        if (hasFocus) holder.itemView.findViewById<TextView>(R.id.tv).scaleX = 1.3F
+                        else holder.itemView.findViewById<TextView>(R.id.tv).scaleX = 1F
+                    }
                 }
             }
             1 -> {
