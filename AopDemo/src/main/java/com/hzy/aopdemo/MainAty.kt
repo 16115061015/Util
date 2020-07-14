b23fcc9d30eb4d9ccae1715a2b59a88b177b54e4
huzeyu <hzy-joel@qq.com>
2020-07-15 00:35:39 +0800

    feat(Aop Demo): 学习切面Aop

---
+++ AopDemo/src/main/java/com/hzy/aopdemo/MainAty.kt
@@ -0,0 +1,18 @@
+package com.hzy.aopdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hzy.aopdemo.R
+/**
+ * User: hzy
+ * Date: 2020/6/25
+ * Time: 4:27 PM
+ * Description:
+ */
+class MainAty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
