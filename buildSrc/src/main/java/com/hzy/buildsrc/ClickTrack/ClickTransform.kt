18b15e1f1adf0ff7bd0b1da8021d77157ecd3dbf
huzeyu <hzy-joel@qq.com>
2020-07-23 20:12:34 +0800

    feat(自定义Transform增量编译):

--- buildSrc/src/main/java/com/hzy/buildsrc/ClickTrack/ClickTransform.kt
+++ buildSrc/src/main/java/com/hzy/buildsrc/ClickTrack/ClickTransform.kt
@@ -1,6 +1,7 @@
 package com.hzy.buildsrc.ClickTrack
 
 import com.android.build.api.transform.*
+import com.android.utils.FileUtils
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import java.io.File
@@ -28,7 +29,7 @@ class ClickTransform : Transform() {
     // 返回true的话表示支持，这个时候可以根据 com.android.build.api.transform.TransformInput 来获得更改、移除或者添加的文件目录或者jar包。
     // 开启则记得在全量删除之前的生出文件
     override fun isIncremental(): Boolean {
-        return false
+        return true
     }
 
     //处理输入文件的范围，范围越小速度越快
@@ -38,6 +39,14 @@ class ClickTransform : Transform() {
 
     //处理过程
     override fun transform(transformInvocation: TransformInvocation) {
+        //获取到输出目录，最后将修改的文件复制到输出目录，这一步必须做不然编译会报错
+        if (isIncremental)
+            incrementalTransform(transformInvocation)
+        else
+            fullIncrementalTransform(transformInvocation)
+    }
+
+    private fun fullIncrementalTransform(transformInvocation: TransformInvocation) {
         //获取到输出目录，最后将修改的文件复制到输出目录，这一步必须做不然编译会报错
         val outputProvider = transformInvocation?.outputProvider
         if (!isIncremental) outputProvider.deleteAll()
@@ -52,12 +61,43 @@ class ClickTransform : Transform() {
             }
 
         }
-
     }
 
+    private fun incrementalTransform(transformInvocation: TransformInvocation){
+        //遍历输入的文件
+        val outputProvider = transformInvocation.outputProvider
+        transformInvocation.inputs?.forEach { inputs ->
+            inputs.directoryInputs.forEach { dir ->
+                val dest = outputProvider?.getContentLocation(
+                        dir.name,
+                        dir.contentTypes,
+                        dir.scopes,
+                        Format.DIRECTORY)
+                dir.changedFiles.forEach{ (file, status) ->
+                    when (status) {
+                        Status.ADDED, Status.CHANGED -> {
+                            traverseDir(dir, outputProvider, false)
+                        }
+                        Status.REMOVED -> {
+                            //删除 输出目录
+                            val outputFile = File(dest, FileUtils.relativePossiblyNonExistingPath(dir.file, file))
+                            FileUtils.deleteIfExists(outputFile)
+                        }
+                    }
+                }
+                copyFile(dir,outputProvider)
+
+            }
+            //处理jar文件
+            inputs.jarInputs.forEach { jar ->
+                traverseJar(jar, outputProvider)
+            }
+
+        }
+    }
 
     private fun traverseJar(jarInput: JarInput, outputProvider: TransformOutputProvider) {
-        //传递给下一个Transform处理
+        //传递给下一个Transform处理  jar修改需要先解压为zip
         val outputJar = outputProvider.getContentLocation(
                 jarInput.name,
                 jarInput.contentTypes,
@@ -68,7 +108,7 @@ class ClickTransform : Transform() {
 
     }
 
-    private fun traverseDir(dirInput: DirectoryInput, outputProvider: TransformOutputProvider?) {
+    private fun traverseDir(dirInput: DirectoryInput, outputProvider: TransformOutputProvider?, copy: Boolean = true) {
         if (dirInput.file.isDirectory) {
             dirInput.file.walk().forEach {
                 if (it.isFile) transformFile(it, dirInput, outputProvider)
@@ -77,6 +117,12 @@ class ClickTransform : Transform() {
             transformFile(dirInput.file, dirInput, outputProvider)
         }
         //传递给下一个Transform处理
+        //复制修改文件
+        if(copy) copyFile(dirInput,outputProvider)
+    }
+
+
+    private fun copyFile(dirInput: DirectoryInput, outputProvider: TransformOutputProvider?) {
         val dest = outputProvider?.getContentLocation(
                 dirInput.name,
                 dirInput.contentTypes,
@@ -84,7 +130,6 @@ class ClickTransform : Transform() {
                 Format.DIRECTORY)
         //复制修改文件
         dest?.let { dirInput.file.copyRecursively(File(it.path), true) }
-
     }
 
     /***
